7afc1a5e911ed95b436b00d0ea11edfa1e5ca0f3
oceankyuubi <oceankyuubi@localhost.localdomain>
2020-07-09 12:44:44 -0400

    added the main module and ignored the dist and egg.info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as readmeFile:
 
 setup(
     name="pipfilemerge",
-    version="0.0.5",
+    version="0.0.6",
     metadata_version="1.0",
     description="Merge the Pipfiles into a single global Pipfile",
     author="Laurkan Rodriguez",
@@ -13,7 +13,7 @@ setup(
     long_description=readmeStr,
     long_description_content_type="text/markdown",
     url="https://github.com/lorkaan/merge-pipfile.git",
-    download_url="https://github.com/lorkaan/pipfilemerge/archive/v0.0.5.tar.gz",
+    download_url="https://github.com/lorkaan/pipfilemerge/archive/v0.0.6.tar.gz",
     install_requires =[
         'toml'
     ],
